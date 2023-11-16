2960600ddddf87148610fca6add9f77bc2f616fa
GlazMichelFranckDodo225 <cda.gmfd@gmail.com>
2023-11-16 12:25:07 +0100

    Non-Primitive Datatypes : Strings and Arrays

---
+++ src/com/dgmf/nonprimitivedatatypes/NonPrimitiveDatatypes.java
@@ -0,0 +1,24 @@
+package com.dgmf.nonprimitivedatatypes;
+
+public class NonPrimitiveDatatypes {
+
+	public static void main(String[] args) {
+		// String is going to reuse the same Object
+		String str1 = "Test 1";
+		System.out.println("The String 1 is : " + str1);
+		
+		// String is going to create a new Object
+		String str2 = new String("Test 2");
+		System.out.println("The String 2 is : " + str2);
+		
+		int[] array1; // Declaration // int array1[];
+		array1 = new int[2]; // Assign a size (number of values is going to store)
+		array1[0] = 0;
+		array1[1] = 1;
+		// array1[2] = 2; // Exception ==> "ArrayIndexOutOfBoundsException"
+		
+		System.out.println(array1); // It will print the reference of the array
+		System.out.println(array1[0]);
+	}
+
+}
